30e344b057d7739ed10825ed1f84e87f983bfa67
Arnold <arnolddonpaul@gmail.com>
2020-06-11 12:14:15 -0400

    populated About's expanded text with appropriate info regarding inhalation, exhalation, and controlling the pattern of breathing. styled a few elements such as hover and the border-bottom of the expanded information

--- src/components/About.js
+++ src/components/About.js
@@ -13,11 +13,30 @@ class About extends Component {
                 <p className="about__text" onClick={() => this.setState({ inhaleExpanded: true })}>inhale deeply through the nose and into the pit of your stomach.</p>
                 {
                     this.state.inhaleExpanded === true && (
-                        <p>TEST</p>
+                        <ul className="about__expandedList">
+                            <li className="about__textExpanded">prevent your shoulders from rising.</li>
+                            <li className="about__textExpanded">expand your stomach outward as you inhale.</li>
+                        </ul>
                     )
                 }
-                <p className="about__text">exhale slowly through your mouth, emptying the lungs completely.</p>
-                <p className="about__text"> when attention drifts, bring it back to your pattern of breathing.</p>
+                <p className="about__text" onClick={() => this.setState({ exhaleExpanded: true })}>exhale slowly through your mouth, emptying the lungs completely.</p>
+                {
+                    this.state.exhaleExpanded === true && (
+                        <ul className="about__expandedList">
+                            <li className="about__textExpanded">your exhale should take longer than your inhale</li>
+                            <li className="about__textExpanded">slow exhalation will promote a calming sensation throughout the body</li>
+                        </ul>
+                    )
+                }
+                <p className="about__text" onClick={() => this.setState({ attentionExpanded: true })}> when attention drifts, bring it back to your pattern of breathing.</p>
+                {
+                    this.state.attentionExpanded === true && (
+                        <ul className="about__expandedList">
+                            <li className="about__textExpanded">inhale for a count of 2 and exhale for a count of 4-6</li>
+                            <li className="about__textExpanded">practice makes better</li>
+                        </ul>
+                    )
+                }
             </div >
         );
     }
